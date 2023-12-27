c010ce6a44ecfc5a6a67c05078943a28453a1354
TruongAN7023 <154291936+TruongAN7023@users.noreply.github.com>
2023-12-27 14:58:01 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,21 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		int number;
+		String nameSchool;
+	}
+	/*
+	 * ten phuong thuc tuan theo the thuc camelcese
+	 */
+	public void show() {
+
+		
+
+	}
+
+}
